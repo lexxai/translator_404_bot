a0661d259c1f3f42bb59beb5474ca2e2603e19ed
lexxai <lexxai@gmail.com>
2025-03-20 20:03:17 +0200

    README Docker

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,5 +1,5 @@
 #/usr/bin/env bash
 
 pushd ..
-docker run -d --name translator_404_bot --rm translator_404_bot
+docker run -d --name translator_404_bot --rm --volume translator_404_bot_storage:/app/storage translator_404_bot
 popd
