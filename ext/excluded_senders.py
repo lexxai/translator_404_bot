b638950ad5100f21ed135c25d02d78b0fc786af7
lexxai <lexxai@gmail.com>
2025-03-23 01:04:55 +0200

    Sessions

--- ext/excluded_senders.py
+++ ext/excluded_senders.py
@@ -1,56 +1,80 @@
 import logging
 import pickle
+from enum import StrEnum
 from pathlib import Path
 
 logger = logging.getLogger("bot." + __name__)
 
 
-class ExcludedSenders:
+class Category(StrEnum):
+    EXCLUDED_SENDERS = "excluded_senders"
+    INFORMED = "informed"
+
+
+class Sessions:
     def __init__(self, storage_path: Path):
-        self.excluded_senders = {}
-        self.excluded_senders_filename = ".excluded_senders.pickle"
+        self.sessions = {Category.EXCLUDED_SENDERS: {}, Category.INFORMED: {}}
+        self.sessions_filename = ".sessions.pickle"
         self.storage_path = storage_path
+
+    @property
+    def excluded_senders(self):
+        return self.sessions[Category.EXCLUDED_SENDERS]
+
+    @property
+    def informed(self):
+        return self.sessions[Category.INFORMED]
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return str(self.excluded_senders)
+        return str(self.sessions)
 
-    def add_excluded_sender(self, sender_id, group_id):
-        self.excluded_senders.setdefault(group_id, set()).add(sender_id)
-        self.save_excluded_senders()
+    def add(self, category: Category, group_id: int, value):
+        self.sessions[category].setdefault(group_id, set()).add(value)
+        self.save()
 
-    def remove_excluded_sender(self, sender_id, group_id):
-        if group_id in self.excluded_senders:
-            self.excluded_senders[group_id].discard(sender_id)
-            self.save_excluded_senders()
+    def remove(self, category: Category, group_id: int, value):
+        self.sessions[category][group_id].discard(value)
+        self.save()
 
-    def is_excluded_sender(self, sender_id, group_id):
-        return (
-            group_id in self.excluded_senders
-            and sender_id in self.excluded_senders[group_id]
+    def is_exist(self, category: Category, group_id: int, value):
+        return value in self.sessions[category][group_id]
+
+    def add_excluded_sender(self, sender_id, group_id: int):
+        self.add(Category.EXCLUDED_SENDERS, group_id, sender_id)
+
+    def remove_excluded_sender(self, sender_id, group_id: int):
+        category = Category.EXCLUDED_SENDERS
+        if group_id in self.sessions[category]:
+            self.remove(category, group_id, sender_id)  # remove
+
+    def is_excluded_sender(self, sender_id, group_id: int):
+        category = Category.EXCLUDED_SENDERS
+        return group_id in self.sessions[category] and self.is_exist(
+            category, group_id, sender_id
         )
 
-    def save_excluded_senders(self, storage_file: str = None):
-        storage_file = storage_file or self.excluded_senders_filename
+    def save(self, storage_file: str = None):
+        storage_file = storage_file or self.sessions_filename
         self.storage_path.mkdir(parents=True, exist_ok=True)
         excluded_senders_path = self.storage_path / storage_file
         try:
             with excluded_senders_path.open("wb") as f:
-                pickle.dump(self.excluded_senders, f)
+                pickle.dump(self.sessions, f)
         except Exception as e:
             logger.error(e)
 
-    def load_excluded_senders(self, storage_file: str = None) -> None:
-        storage_file = storage_file or self.excluded_senders_filename
+    def load(self, storage_file: str = None) -> None:
+        storage_file = storage_file or self.sessions_filename
         if not self.storage_path.exists():
             return None
-        excluded_senders_path = self.storage_path / storage_file
-        if excluded_senders_path.exists():
+        sessions_path = self.storage_path / storage_file
+        if sessions_path.exists():
             try:
-                with excluded_senders_path.open("rb") as f:
-                    self.excluded_senders = pickle.load(f)
-                    logger.debug(f"Loaded: {self.excluded_senders}")
+                with sessions_path.open("rb") as f:
+                    self.sessions = pickle.load(f)
+                    logger.debug(f"Loaded: {self.sessions}")
             except Exception as e:
                 logger.error(e)
